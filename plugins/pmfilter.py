39458f4e7f769add50e05ac1e9adb030d79559a5
Deendayal_dhakad <151893234+Deendayal403@users.noreply.github.com>
2024-11-20 04:38:42 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -80,7 +80,7 @@ async def get_shortlink(url):
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     await message.react(emoji=random.choice(REACTIONS), big=True)
-   # await mdb.update_top_messages(message.from_user.id, message.text)
+    await mdb.update_top_messages(message.from_user.id, message.text)
     k = await manual_filters(client, message)
     if k == False:
         await auto_filter(client, message)
@@ -88,8 +88,8 @@ async def give_filter(client, message):
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
-    await message.react(emoji=random.choice(REACTIONS), big=True)
-   # await mdb.update_top_messages(message.from_user.id, message.text)
+    #await message.react(emoji=random.choice(REACTIONS), big=True)
+    await mdb.update_top_messages(message.from_user.id, message.text)
     content = message.text
     user = message.from_user.first_name
     user_id = message.from_user.id
